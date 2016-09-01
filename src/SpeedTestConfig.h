dafd6fe1f34223216b98cec2edba981ca04768c2
Martin Schröder <mkschreder.uk@gmail.com>
2016-09-01 23:00:04 +0200

    use autotools and also make output clean

---
+++ src/SpeedTestConfig.h
@@ -0,0 +1,16 @@
+// The configured options and settings for SpeedTest
+#define SpeedTest_VERSION_MAJOR 1
+#define SpeedTest_VERSION_MINOR 5
+#define SpeedTest_SYSTEM_PROCESSOR "x86_64"
+#define SpeedTest_SYSTEM "Linux-3.19.0-32-generic"
+#define SpeedTest_AUTHOR "Francesco Laurita <francesco.laurita@gmail.com>"
+#define SpeedTest_HOME_PAGE "https://github.com/taganaka/SpeedTest"
+
+#define SPEED_TEST_USER_AGENT "Mozilla/5.0 Linux-3.19.0-32-generic; U; x86_64; en-us (KHTML, like Gecko) SpeedTest++/1.5"
+#define SPEED_TEST_SERVER_LIST_URL "http://www.speedtest.net/speedtest-servers.php"
+#define SPEED_TEST_SERVER_QUALITY_LIST_URL "http://www.pingtest.net/pingtest-config.php"
+
+#define SPEED_TEST_IP_INFO_API_URL "http://speedtest.ookla.com/api/ipaddress.php"
+#define SPEED_TEST_API_URL "http://www.speedtest.net/api/api.php"
+#define SPEED_TEST_API_REFERER "http://c.speedtest.net/flash/speedtest.swf"
+#define SPEED_TEST_API_KEY "297aae72"
--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ void usage(const char* name){
     std::cout << "\t--download  Perform download test only. It includes latency test\n";
     std::cout << "\t--upload    Perform upload test only. It includes latency test\n";
     std::cout << "\t--share     Generate and provide a URL to the speedtest.net share results image\n";
+    std::cout << "\t--verbose   Show verbose output\n";
 }
 
 int main(const int argc, const char **argv) {
@@ -28,14 +29,16 @@ int main(const int argc, const char **argv) {
     bool upload_only   = false;
     bool latency_only  = false;
     bool share         = false;
-
+	bool verbose		= false; 
     std::vector<std::string> options;
 
     for (int i = 0; i < argc; i++)
         options.push_back(std::string(argv[i]));
 
-    banner();
-    std::cout << std::endl;
+    if(verbose) {
+		banner();
+    	std::cout << std::endl;
+	}
 
     if (std::find(options.begin(), options.end(), "--help") != options.end()) {
         usage(argv[0]);
@@ -54,6 +57,9 @@ int main(const int argc, const char **argv) {
     if (std::find(options.begin(), options.end(), "--share") != options.end())
         share = true;
 
+    if (std::find(options.begin(), options.end(), "--verbose") != options.end())
+        verbose = true;
+
 
     auto sp = SpeedTest();
     IPInfo info;
@@ -62,55 +68,59 @@ int main(const int argc, const char **argv) {
         return EXIT_FAILURE;
     }
 
-    std::cout << "IP: " << info.ip_address
-        << " ( " << info.isp << " ) "
-        << "Location: [" << info.lat << ", " << info.lon << "]" << std::endl;
+	if(verbose) {
+		std::cout << "IP: " << info.ip_address
+			<< " ( " << info.isp << " ) "
+			<< "Location: [" << info.lat << ", " << info.lon << "]" << std::endl;
 
-    std::cout << "Finding fastest server... " << std::flush;
+		std::cout << "Finding fastest server... " << std::flush;
+	}
+
     auto serverList = sp.serverList();
     if (serverList.empty()){
         std::cerr << "Unable to download server list. Try again later" << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << serverList.size() << " Servers online" << std::endl;
+
+	if(verbose)
+    	std::cout << serverList.size() << " Servers online" << std::endl;
 
 
     ServerInfo serverInfo = sp.bestServer(10, [](bool success) {
-        std::cout << (success ? '.' : '*') << std::flush;
+        //if(verbose) std::cout << (success ? '.' : '*') << std::flush;
     });
 
-    std::cout << std::endl;
-    std::cout << "Server: " << serverInfo.name
-        << " by " << serverInfo.sponsor
-        << " (" << serverInfo.distance << " km from you): "
-        << sp.latency() << " ms" << std::endl;
-    std::cout << "Ping: " << sp.latency() << " ms." << std::endl;
+    if(verbose) std::cout << std::endl;
+
+    std::cout << "server " << serverInfo.name << std::endl
+        << "sponsor " << serverInfo.sponsor << std::endl
+        << "server_distance_km " << serverInfo.distance << std::endl
+        << "server_latency_ms " << sp.latency() << std::endl;
 
     long jitter = 0;
-    std::cout << "Jitter: " << std::flush;
     if (sp.jitter(serverInfo, jitter)){
-        std::cout << jitter << " ms." << std::endl;
-    } else {
-        std::cout << "Jitter measurement is unavailable at this time." << std::endl;
-    }
+    	std::cout << "jitter_ms " << jitter << std::endl;
+    } 
 
-    std::cout << "Finding fastest server for packet loss measurement... " << std::flush;
+    if(verbose) std::cout << "Finding fastest server for packet loss measurement... " << std::flush;
     auto serverQualityList = sp.serverQualityList();
 
     if (serverQualityList.empty()){
         std::cerr << "Unable to download server list. Packet loss analysis is not available at this time" << std::endl;
     } else {
-        std::cout << serverQualityList.size() << " Ping hosts online" << std::endl;
+        if(verbose) std::cout << serverQualityList.size() << " Ping hosts online" << std::endl;
         ServerInfo serverQualityInfo = sp.bestQualityServer(5, [](bool success){
-            std::cout << (success ? '.' : '*') << std::flush;
+            //if(verbose) std::cout << (success ? '.' : '*') << std::flush;
         });
-        std::cout << std::endl;
-        std::cout << "Server: " << serverQualityInfo.name
-        << " by " << serverQualityInfo.sponsor
-        << " (" << serverQualityInfo.distance << " km from you): " << std::endl;
+		if(verbose) {	
+			std::cout << std::endl;
+			std::cout << "Server: " << serverQualityInfo.name
+			<< " by " << serverQualityInfo.sponsor
+			<< " (" << serverQualityInfo.distance << " km from you): " << std::endl;
+		}
         int ploss = 0;
         if (sp.packetLoss(serverQualityInfo, ploss)){
-            std::cout << "Packets loss: " << ploss << std::endl;
+            std::cout << "packet_loss " << ploss << std::endl;
         }
     }
 
@@ -118,43 +128,45 @@ int main(const int argc, const char **argv) {
         return EXIT_SUCCESS;
 
 
-    std::cout << "Determine line type (" << preflightConfigDownload.concurrency << ") "  << std::flush;
+    if(verbose) std::cout << "Determine line type (" << preflightConfigDownload.concurrency << ") "  << std::flush;
     double preSpeed = 0;
     if (!sp.downloadSpeed(serverInfo, preflightConfigDownload, preSpeed, [](bool success){
-        std::cout << (success ? '.' : '*') << std::flush;
+        //if(verbose) std::cout << (success ? '.' : '*') << std::flush;
     })){
         std::cerr << "Pre-flight check failed." << std::endl;
         return EXIT_FAILURE;
     }
-    std::cout << std::endl;
+    if(verbose) std::cout << std::endl;
 
     TestConfig uploadConfig   = slowConfigUpload;
     TestConfig downloadConfig = slowConfigDownload;
     if (preSpeed <= 4){
-        std::cout << "Very-slow-line line type detected: profile selected slowband" << std::endl;
+		std::cout << "detected_line_type " << "slowband" << std::endl; 
     } else if (preSpeed > 4 && preSpeed <= 30){
-        std::cout << "Buffering-lover line type detected: profile selected narrowband" << std::endl;
+		std::cout << "detected_line_type " << "narrowband" << std::endl; 
         downloadConfig = narrowConfigDownload;
         uploadConfig   = narrowConfigUpload;
     } else if (preSpeed > 30 && preSpeed < 150) {
-        std::cout << "Broadband line type detected: profile selected broadband" << std::endl;
+		std::cout << "detected_line_type " << "broadband" << std::endl; 
         downloadConfig = broadbandConfigDownload;
         uploadConfig   = broadbandConfigUpload;
     } else if (preSpeed >= 150) {
-        std::cout << "Fiber / Lan line type detected: profile selected fiber" << std::endl;
+		std::cout << "detected_line_type " << "fiber" << std::endl; 
         downloadConfig = fiberConfigDownload;
         uploadConfig   = fiberConfigUpload;
     }
 
     if (!upload_only){
-        std::cout << std::endl;
-        std::cout << "Testing download speed (" << downloadConfig.concurrency << ") "  << std::flush;
+        if(verbose) {
+			std::cout << std::endl;
+        	std::cout << "Testing download speed (" << downloadConfig.concurrency << ") "  << std::flush;
+		}
         double downloadSpeed = 0;
         if (sp.downloadSpeed(serverInfo, downloadConfig, downloadSpeed, [](bool success){
-            std::cout << (success ? '.' : '*') << std::flush;
+            //if(verbose) std::cout << (success ? '.' : '*') << std::flush;
         })){
-            std::cout << std::endl;
-            std::cout << "Download: " << downloadSpeed << " Mbit/s" << std::endl;
+            if(verbose) std::cout << std::endl;
+            std::cout << "download_mbits " << downloadSpeed << std::endl;
         } else {
             std::cerr << "Download test failed." << std::endl;
             return EXIT_FAILURE;
@@ -165,13 +177,13 @@ int main(const int argc, const char **argv) {
         return EXIT_SUCCESS;
 
 
-    std::cout << "Testing upload speed (" << uploadConfig.concurrency << ") "  << std::flush;
+    if(verbose) std::cout << "Testing upload speed (" << uploadConfig.concurrency << ") "  << std::flush;
     double uploadSpeed = 0;
     if (sp.uploadSpeed(serverInfo, uploadConfig, uploadSpeed, [](bool success){
-        std::cout << (success ? '.' : '*') << std::flush;
+        //if(verbose) std::cout << (success ? '.' : '*') << std::flush;
     })){
-        std::cout << std::endl;
-        std::cout << "Upload: " << uploadSpeed << " Mbit/s" << std::endl;
+        if(verbose) std::cout << std::endl;
+        std::cout << "upload_mbits " << uploadSpeed << std::endl;
     } else {
         std::cerr << "Upload test failed." << std::endl;
         return EXIT_FAILURE;
@@ -181,7 +193,7 @@ int main(const int argc, const char **argv) {
     if (share){
         std::string share_it;
         if (sp.share(serverInfo, share_it)){
-            std::cout << "Results image: " << share_it << std::endl;
+            std::cout << "results_image_url " << share_it << std::endl;
         }
     }
 
